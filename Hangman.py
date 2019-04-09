72d16745e0ba87111fbf837051a8b7ce7f1233a3
CodyMitchellCPA <49317392+CodyMitchellCPA@users.noreply.github.com>
2019-04-09 14:07:45 -0400

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -15,36 +15,38 @@ uniqueChars = set(answer.lower())
 guessedChars = set()
 correctChars = set()
 currentWord = ""
-#currentWord
+
 for i in range(0, len(answer)):
     if answer[i] == ' ':
         currentWord = currentWord + ' '
     else:
-        currentWord = currentWord + '$'
+        currentWord = currentWord + '_'
 
 
 #allow multi-letter words w/out user having to guess a space
 if ' ' in uniqueChars:
     correctChars.add(' ')
-print("Attempts left: " + str(attemptsLeft))
-print("Current word: " + currentWord)
+
 
 #game loops until either no attempts left, or player has guessed all of the correct chars
 while attemptsLeft != 0 and correctChars != uniqueChars:
-    letter = input("Enter letter here: ").lower()
+    print("Attempts left: " + str(attemptsLeft))
+    print("Current word: " + currentWord)
+    letter = input("Enter letter here: ")
     #input must be one char, and player must not have guessed it before
     while len(letter) != 1:
-        letter = input("Please enter only one letter: ").lower()
+        letter = input("Please enter only one letter: ")
+    letter = letter.lower()
     while letter in guessedChars:
-        letter = input("You've already guessed that letter. Please try again: ").lower()
+        letter = input("You've already guessed that letter. Please try again: ")
+    letter = letter.lower()
     guessedChars.add(letter)
     if letter in uniqueChars:
         correctChars.add(letter)
         #updateBoard(letter)
     else:
         attemptsLeft = attemptsLeft - 1
-    print("Attempts left: " + str(attemptsLeft))
-    print("Current word: " + currentWord)
+    
         #drawNext()
 if attemptsLeft == 0:
     print("You lose. Please try again.")
